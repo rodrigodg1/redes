e723e50f4fac8c147e8a914f5d3976fd7ac6e472
Rodrigo <rodrigopba1@gmail.com>
2021-05-07 21:59:18 -0300

    Organização dos Diretórios

--- Sockets/Java/TCPServer.java
+++ Sockets/Java/TCPServer.java
@@ -1,29 +1,29 @@
 import java.io.*;
 import java.net.*;
 
-
-
 class TCPServer {
 
- public static void main(String argv[]) throws Exception {
+    public static void main(String argv[]) throws Exception {
+        String clientSentence;
+        String capitalizedSentence;
 
-  String clientSentence;
-  String capitalizedSentence;
-  ServerSocket welcomeSocket = new ServerSocket(6789);
+        ServerSocket welcomeSocket = new ServerSocket(6789);
 
-  while (true) {
+        while (true) {
 
-   Socket connectionSocket = welcomeSocket.accept();
-   BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            System.out.println("\nListening... ");
 
-   clientSentence = inFromClient.readLine();
-   System.out.println("Received: " + clientSentence);
-   capitalizedSentence = clientSentence.toUpperCase() + 'n';
-   outToClient.writeBytes(capitalizedSentence);
+            Socket connectionSocket = welcomeSocket.accept();
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-  }
+            clientSentence = inFromClient.readLine();
 
- }
+            System.out.println("Received: " + clientSentence);
 
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
 }
